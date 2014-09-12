6f73235933a5bfad0de3df28e386226ee429e352
Claudio Fahey <claudio.fahey@emc.com>
2014-09-12 12:27:08 -0400

    added more scripts

---
+++ onefs/isilon_create_cdh_users_for_nfs.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+zone=System
+gid_base=601
+uid_base=601
+
+gid=$gid_base
+for group in hdfs mapred hbase hive yarn oozie sentry impala spark hue sqoop2 solr sqoop httpfs llama zookeper flume sample hadoop supergroup admin; do
+    isi auth groups create $group --zone $zone --provider local --gid $gid
+    gid=$(($gid + 1))
+done
+
+uid=$uid_base
+for user in hdfs mapred hbase hive yarn oozie sentry impala spark hue sqoop2 solr sqoop httpfs llama zookeper flume sample admin; do    
+    isi auth users create $user --zone $zone --provider local --uid $uid --primary-group $user
+    uid=$(($uid + 1))
+done
+
+isi auth groups modify hadoop --add-user hdfs --zone $zone
+isi auth groups modify hadoop --add-user yarn --zone $zone
+isi auth groups modify hadoop --add-user mapred --zone $zone
+isi auth groups modify supergroup --add-user hdfs --zone $zone
+isi auth groups modify supergroup --add-user yarn --zone $zone
+isi auth groups modify supergroup --add-user mapred --zone $zone
+
